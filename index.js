2d1e9ae2a07fa051047487586b2accec3c857ad9
Emre <ahmet.emre.cakmak@yandex.com>
2024-10-17 15:51:58 +0300

    fix

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
     origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
