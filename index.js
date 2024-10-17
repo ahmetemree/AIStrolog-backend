0962ab7d6fe1acd985144b017379438d0995cc1d
Emre <ahmet.emre.cakmak@yandex.com>
2024-10-17 15:48:47 +0300

    fix again

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
 
@@ -35,8 +34,6 @@ const connect = async () => {
   }
 };
 app.use(clerkMiddleware())
-
-app.use(requireAuth());
 
 app.get("/deneme",requireAuth({signInUrl: process.env.CLIENT_URL + '/sign-in'}), async (req, res,) => {
   res.send("deneme");
