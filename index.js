f4b8ad002acf6e76467a01e6731da7ac8c3eff40
Emre <ahmet.emre.cakmak@yandex.com>
2024-10-17 15:28:37 +0300

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
